005fd347ceacde24e082fdc71ebd7773e9d75e42
rohanp95 <rohan.pereira95@gmail.com>
2026-06-01 15:54:40 +0530

    Updated scoring

--- script.js
+++ script.js
@@ -521,7 +521,7 @@ function updateScores(answer) {
 
     if (item === "Gender Equality") {
 
-      productScores.sfcGender += 5;
+      productScores.sfcGender += 10;
       productScores.SFCpathwayz += 3;
       productScores.shakti += 4;
       productScores.pathwayz += 1;
@@ -602,7 +602,7 @@ function updateScores(answer) {
 
     if (item === "Women & girls") {
 
-      productScores.sfcGender += 8;
+      productScores.sfcGender += 12;
       productScores.SFCpathwayz += 4;
       productScores.shakti += 3;
       productScores.pathwayz += 1;
